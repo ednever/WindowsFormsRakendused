1e73a7090d64cb85437ef772d6679e2789b34cf1
opilane <opilane@HP-CZC6297BBL>
2022-09-28 15:49:01 +0300

    new form

--- WindowsFormsRakendused/Form2.Designer.cs
+++ WindowsFormsRakendused/Form2.Designer.cs
@@ -33,11 +33,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
             // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            //this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
@@ -61,27 +64,49 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Alef", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label3.Location = new System.Drawing.Point(50, 75);
+            this.label3.Location = new System.Drawing.Point(38, 75);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 50);
             this.label3.TabIndex = 2;
             this.label3.Text = "?";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Alef", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(170, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Alef", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(236, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 50);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "?";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Alef", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label6.Location = new System.Drawing.Point(104, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 50);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "+";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.MaximizeBox = false;
-            this.Name = "Form2";
-            this.Text = "Math Quiz";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            
 
         }
 
@@ -91,5 +116,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- WindowsFormsRakendused/Form2.cs
+++ WindowsFormsRakendused/Form2.cs
@@ -12,21 +12,90 @@ namespace WindowsFormsRakendused
 {
     public partial class Form2 : Form
     {
+        TableLayoutPanel tableLayoutPanel1;
+        Timer timer;
+        Label l;
+        string l_nimed[,];
         public Form2()
         {
-            InitializeComponent();
+            this.Text = "Math Quiz";
+            this.Name = "Form2";
+            this.ClientSize = new Size(800, 450);
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+
+            tableLayoutPanel1 = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+            };
+
+
+            l_nimed = new string[5, 4];
             //for (int i = 0; i < 4; i++)
             //{
+            //    tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Persent, 25F));
             //    for (int j = 0; j < 5; j++)
             //    {
-                    
+            //        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Persent, 20F));
+            //        var l_nimi = "L" + j.ToString(); + i.ToString();
+            //        l_nimed[j, i] = l_nimi;
+            //        if (j == 1)
+            //        {
+            //            text = tehed[i]; //tehed = new string[4] {"+","-","/","*"}
+            //        }
+            //        else if (j == 3)
+            //        {
+            //            text = "=";
+            //        }
+            //        else if (j == 4)
+            //        {
+            //            text = "vastus";
+            //        }
+            //        else
+            //        {
+            //            text = l_nimi;
+            //        }
+            //        Label l = new Label { Text = text };
+            //        TableLayoutPanel.Controls.Add(l, j, i);
             //    }
             //}
+            this.Controls.Add(tableLayoutPanel1);
         }
-
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            if (CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
         }
     }
 }
